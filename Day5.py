fe7e920548313e86c5f037703fd0dcd448f19a10
fish <yxzjerryfish@hotmail.com>
2019-10-17 22:19:52 +0800

    第五课

---
+++ Day5.py
@@ -0,0 +1,90 @@
+"""
+
+Author: Fish Paradise
+@date : 2019/10/17 22:13
+"""
+
+for num in range(100, 1000):
+    low = num % 10
+    mid = num // 10 % 10
+    high = num // 100
+    if num == low ** 3 + mid ** 3 + high ** 3:
+        print(num)
+
+num = int(input('num = '))
+reversed_num = 0
+while num > 0:
+    reversed_num = reversed_num * 10 + num % 10
+    num //= 10
+print(reversed_num)
+
+for x in range(0, 20):
+    for y in range(0, 33):
+        z = 100 - x - y
+        if 5 * x + 3 * y + z / 3 == 100:
+            print('公鸡: %d只, 母鸡: %d只, 小鸡: %d只' % (x, y, z))
+
+from random import randint
+
+# money = 1000
+# while money > 0:
+#     print('你的总资产为:', money)
+#     needs_go_on = False
+#     while True:
+#         debt = int(input('请下注: '))
+#         if 0 < debt <= money:
+#             break
+#     first = randint(1, 6) + randint(1, 6)
+#     print('玩家摇出了%d点' % first)
+#     if first == 7 or first == 11:
+#         print('玩家胜!')
+#         money += debt
+#     elif first == 2 or first == 3 or first == 12:
+#         print('庄家胜!')
+#         money -= debt
+#     else:
+#         needs_go_on = True
+#     while needs_go_on:
+#         needs_go_on = False
+#         current = randint(1, 6) + randint(1, 6)
+#         print('玩家摇出了%d点' % current)
+#         if current == 7:
+#             print('庄家胜')
+#             money -= debt
+#         elif current == first:
+#             print('玩家胜')
+#             money += debt
+#         else:
+#             needs_go_on = True
+# print('你破产了, 游戏结束!')
+
+a = 0
+b = 1
+for _ in range(20):
+    a, b = b, a + b
+    print(a, end=' ')
+
+print('\n************************************')
+
+import math
+
+for num in range(1, 10000):
+    result = 0
+    for factor in range(1, int(math.sqrt(num)) + 1):
+        if num % factor == 0:
+            result += factor
+            if factor > 1 and num // factor != factor:
+                result += num // factor
+    if result == num:
+        print(num)
+
+import math
+
+for num in range(2, 100):
+    is_prime = True
+    for factor in range(2, int(math.sqrt(num)) + 1):
+        if num % factor == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=' ')
